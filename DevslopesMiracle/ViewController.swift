0c516a49980c2d4818f803cd74baadb35624b5f8
Kerwin Londono <klondono@Kerwins-MacBook-Pro.local>
2017-08-23 15:18:07 -0400

    Initial Commit

---
+++ DevslopesMiracle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevslopesMiracle
+//
+//  Created by Kerwin Londono on 8/23/17.
+//  Copyright © 2017 Kerwin Londono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
